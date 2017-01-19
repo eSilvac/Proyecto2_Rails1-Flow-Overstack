96d75b2bbcb5d05c5c6d613e23e9ffdb0bf20fbf
eSilvac <esilva1995@gmail.com>
2017-01-19 01:11:37 -0500

    Se crea el modelo de questions, se crean las vistas del index y de crear

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,23 @@
 class QuestionsController < ApplicationController
-  def index
-  end
+ 	def index
+  		@questions = Question.all
+  	end
+
+  	def new
+  		@question = Question.new
+  	end
+
+  	def create
+  		@question = Question.create(question_params)
+  		if @question.save
+  			redirect_to questions_path
+  		else
+  			render :new
+  		end
+  	end
+
+  	private
+	  	def question_params
+	  		params.require(:question).permit(:title, :body)
+	  	end
 end
