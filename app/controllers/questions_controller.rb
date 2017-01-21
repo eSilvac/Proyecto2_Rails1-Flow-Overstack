c0386b5aa4f8452bb2db3ee90fabd4626e2f7c45
eSilvac <esilva1995@gmail.com>
2017-01-21 03:26:34 -0500

    Se hace la asociacion de User y question, se crea la vista de edit y se comienza a crear la vista de show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+ 	before_action :authenticate_user!, except: [:index, :show]
+ 	
  	def index
   		@questions = Question.all
   	end
@@ -9,11 +11,29 @@ class QuestionsController < ApplicationController
 
   	def create
   		@question = Question.create(question_params)
+  		@question.user = current_user
   		if @question.save
   			redirect_to questions_path
   		else
   			render :new
   		end
+  	end
+
+  	def show
+  		@question = Question.find(params[:id])
+  	end
+
+  	def edit
+  		@question = Question.find(params[:id])
+  	end
+
+  	def update
+  		@question = Question.find(params[:id])
+		if @question.update(question_params)
+			redirect_to @question
+		else
+			render :edit
+		end  		
   	end
 
   	private
