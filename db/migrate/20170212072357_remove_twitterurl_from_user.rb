c3ff57d0c0729c67545143b1cc13c4633c435075
eSilvac <esilva1995@gmail.com>
2017-02-18 13:46:21 -0500

    Intentado corregir las migraciones

--- db/migrate/20170212072357_remove_twitterurl_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTwitterurlFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :twitterurl, :string
-  end
-end
--- db/migrate/20170218181947_rename_twitter_in_user_to_twitterurl.rb
+++
@@ -1,7 +0,0 @@
-class RenameTwitterInUserToTwitterurl < ActiveRecord::Migration[5.0]
-  def change
-
-    rename_column :users, :twitter, :twitterurl
-  
-  end
-end
